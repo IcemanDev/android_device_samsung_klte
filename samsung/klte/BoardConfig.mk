84be18169e4e394b447e6a9c507bfffbba844aee
IceMan <cammaratamichele1966@gmail.com>
2019-05-23 12:47:43 +0200

    Add files via upload

---
+++ samsung/klte/BoardConfig.mk
@@ -0,0 +1,123 @@
+#BoardConfig.mk by IceMan
+
+LOCAL_PATH := device/samsung/klte
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/zImage
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/recovery.fstab
+TARGET_PREBUILT_DTB := $(LOCAL_PATH)/dtb.img
+
+USE_CAMERA_STUB := true
+
+TARGET_NO_BOOTLOADER := true
+TARGET_BOOTLOADER_BOARD_NAME := MSM8974
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8974
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno330
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DQCOM_HARDWARE -DNO_SECURE_DISCARD
+
+# Architecture
+TARGET_CPU_VARIANT := krait
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+##########################################################################################################
+# Partitions & Kernel CALCOLO AUTOMATICO
+
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_BOOTIMAGE_PARTITION_SIZE := 13631488
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 15728640
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2548039680
+BOARD_CACHEIMAGE_PARTITION_SIZE := 209715200
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 12442369024 # 12442385408 - 16384 (footer)
+BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.bootdevice=msm_sdcc.1 user_debug=31 msm_rtb.filter=0x3F androidboot.selinux=permissive
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x02000000
+BOARD_TAGS_OFFSET := 0x01E00000
+##########################################################################################################
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
+
+# Use this flag if the board has a ext4 partition larger than 2gb
+BOARD_HAS_LARGE_FILESYSTEM := true
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/bootimg.mk
+
+# TWRP specific build flags
+TW_THEME := portrait_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TARGET_RECOVERY_QCOM_RTC_FIX := true
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/msm_dwc3/f9200000.dwc3/gadget/lun%d/file"
+TW_BRIGHTNESS_PATH := "/sys/devices/mdp.0/qcom\x2cmdss_fb_primary.191/leds/lcd-backlight/brightness"
+TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone1/temp"
+TW_MTP_DEVICE := "/dev/mtp_usb"
+
+# Encryption support
+TW_INCLUDE_CRYPTO := true
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_KEYMASTER_WAIT_FOR_QSEE := true
+
+# CUSTOMIZATION by IceMan
+
+BOARD_HAS_NO_REAL_SDCARD := false
+TW_DEFAULT_EXTERNAL_STORAGE := true
+
+# Disable Mouse Cursor
+TW_INPUT_BLACKLIST := "hbtp_vm"
+
+# Asian region languages
+TW_EXTRA_LANGUAGES := false
+
+# Exclude Supersu
+TW_EXCLUDE_SUPERSU := true
+
+# View button bootloader
+TW_NO_REBOOT_BOOTLOADER := true
+
+# View button downloadmode - true only samsung
+TW_HAS_DOWNLOAD_MODE := true
+
+# Default Brightness (TW_DEFAULT_BRIGHTNESS=255-->100%; TW_DEFAULT_BRIGHTNESS=218-->85%)
+TW_DEFAULT_BRIGHTNESS := 255
+TW_MAX_BRIGHTNESS := 255
+
+# Use ro.product.model
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+
+# NTFS support
+TW_INCLUDE_NTFS_3G := true
+
+# Exfat support
+TW_NO_EXFAT := false
+TW_NO_EXFAT_FUSE := false
+
+# Set language default
+TW_DEFAULT_LANGUAGE := en
+
+# For Version TWRP
+#TW_SPECIFIC_VERSION_STR := "X.X.X"
+TW_DEVICE_VERSION := "for SM-G900F by IceMan"
+
+# For time custom
+TW_SPECIFIC_TIME_ZONE_VAR := "CET-1CEST,M3.5.0,M10.5.0"
+TW_SPECIFIC_TIME_ZONE_GUISEL := "CET-1;CEST,M3.5.0,M10.5.0"
+
+# For H24 hours
+TW_SPECIFIC_MILITARY_TIME := "1"
+
+# For Screen_timeout_secs
+TW_SCREEN_TIMEOUT_SECS := "120"
+
+#For reverse navbar (default "0")
+TW_SPECIFIC_SAMSUNG_NAVBAR := "0"
